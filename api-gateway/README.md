20b2a0ed3834540b924082a2c61bf9800c5f2b32
ghf704694314 <704694314@qq.com>
2017-12-14 12:35:25 +0800

    api-gateway增加参数配置

--- api-gateway/README.md
+++ api-gateway/README.md
@@ -1,11 +1,8 @@
 ##路由功能
 建议使用面向服务的路由
-
 path映射到某个具体的服务，url则交给Eureka的服务发现机制
 去自动维护。
-
 启动eureka-sever,eureka-client,feign-consumer,api-gateway
-
 访问http://localhost:5555/api-a/hello,会被映射到eureka-client的/hello上
 http://localhost:5555/api-b/feign-consumer,会被映射到feign-consumer的/feign-consumer上
     
@@ -16,6 +13,16 @@ http://localhost:5555/api-b/feign-consumer,会被映射到feign-consumer的/feig
 才能启动该过滤器。
 http://localhost:5555/api-a/hello?accessToken=token 通过accessFilter进行拦截之后
 路由到eureka-client的/hello
+
+##Cookie与信息头
+默认情况下，Zuul在请求路由时会通过默认的zuul.sensitiveHeaders参数定义,过滤Cookie、
+Set-Cookie、Authorization三个属性。
+可以通过设置全局zuul.sensitiveHeaders为空来覆盖默认值，但不推荐
+使用下面两种
+1. 对指定路由开启自定义敏感头
+zuul.routes.<router>.customSensitiveHeaders=true
+2. 将指定路由的敏感头设置为空
+zuul.routes.<router>.sensitiveHeaders=
 
 ##总结Zuul
 1. 作为系统的唯一入口,屏蔽了系统内部各个微服务的细节
--- api-gateway/src/main/java/com/gaohf/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/gaohf/ApiGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
@@ -15,6 +16,15 @@ public class ApiGatewayApplication {
 	@Bean
 	public AccessFilter accessFilter(){
 		return new AccessFilter();
+	}
+
+	/**
+	 * 创建与版本号对应的路由规则 如:/appName-v1 -->/v1/appName
+	 * @return
+	 */
+	@Bean
+	public PatternServiceRouteMapper serviceRouteMapper(){
+		return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)","${version}/${name}");
 	}
 
 	public static void main(String[] args) {
